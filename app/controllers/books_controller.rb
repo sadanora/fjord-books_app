110864c6be6627dcbcc968f2d86dc54ccea45c1a
sadanora <sadanora.pi@gmail.com>
2021-12-24 10:20:59 +0900

    clean: 不要ファイル削除 / 不要な記述削除
    respond_toメソッドの削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,11 @@ class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
 
   # GET /books
-  # GET /books.json
   def index
     @books = Book.all
   end
 
   # GET /books/1
-  # GET /books/1.json
   def show; end
 
   # GET /books/new
@@ -22,43 +20,28 @@ class BooksController < ApplicationController
   def edit; end
 
   # POST /books
-  # POST /books.json
   def create
     @book = Book.new(book_params)
-
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: t('books.create.success') }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.save
+      redirect_to @book, notice: t('books.create.success')
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: t('books.update.success') }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    if @book.update(book_params)
+      redirect_to @book, notice: t('books.update.success')
+    else
+      render :edit
     end
   end
 
   # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: t('books.destroy.success') }
-      format.json { head :no_content }
-    end
+    redirect_to books_url, notice: t('books.destroy.success')
   end
 
   private

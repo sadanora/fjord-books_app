6fb11e4bec5030efe1daaf71614de0c0a8956c4b
sadanora <sadanora.pi@gmail.com>
2022-01-22 10:53:40 +0900

    add: users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'books#index'
-  devise_for :users
   resources :books
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  resources :users, :only => [:index, :show]
+  get '/mypage', to: 'mypage#show'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end

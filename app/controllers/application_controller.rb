c6b5d08bb1079ab8759999af5aec688eb26169f8
sadanora <sadanora.pi@gmail.com>
2022-01-29 11:16:43 +0900

    change: i18n
    デフォルトのロケール設定の書き方を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action do
-    I18n.locale = :ja
+  before_action :set_locale
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end

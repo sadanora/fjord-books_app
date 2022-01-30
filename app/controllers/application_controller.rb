cb22a0045a423c9f3c52eead764936ab35368e8d
sadanora <sadanora.pi@gmail.com>
2022-01-30 23:34:37 +0900

    clean: set_localeを改めて設定する必要がないため削除
    application_controllerからset_localeを削除した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :set_locale
   before_action :authenticate_user!
-
-  def default_url_options
-    { locale: I18n.locale }
-  end
-
-  private
-
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
 end

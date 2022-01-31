cd90553f24c1f77ce27efeebf9f7f760aec869a1
sadanora <sadanora.pi@gmail.com>
2022-01-31 20:18:18 +0900

    rubocop
    許容する警告を無効化

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
-
-  def create; end
-
-  def update; end
+  before_action :configure_sign_up_params, only: [:create] # rubocop:disable Rails/LexicallyScopedActionFilter
+  before_action :configure_account_update_params, only: [:update] # rubocop:disable Rails/LexicallyScopedActionFilter
 
   # GET /resource/sign_up
   # def new

d958c46eac656d7229cde49998d8eee6e193ca48
sadanora <sadanora.pi@gmail.com>
2022-02-12 01:42:41 +0900

    change: updateアクションをカスタマイズ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    super
+    if account_update_params[:avatar].present?
+      resource.avatar.attach(account_update_params[:avatar])
+    end
+  end
 end

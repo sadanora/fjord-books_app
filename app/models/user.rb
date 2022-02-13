2292e47bf033a9bd53ad5d209a0f1e3f1c129378
sadanora <sadanora.pi@gmail.com>
2022-02-13 18:28:54 +0900

    add: 画像のリサイズ実装
    画像フォーマットのバリデーションも追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  validates :avatar, content_type: { in: %w[avatar/jpg avatar/png avatar/gif] }
+
+  # リサイズ済みのプロフィール画像を返す
+  def display_avatar
+    avatar.variant(resize_to_limit: [64, 64])
+  end
 end

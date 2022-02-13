f1ff5e6d1e25e45216da452fe407448e859d2d9a
sadanora <sadanora.pi@gmail.com>
2022-02-13 20:07:45 +0900

    fix: 画像のバリデーション修正
    content_typeの指定方法を間違っていたので修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
-  validates :avatar, content_type: { in: %w[avatar/jpg avatar/png avatar/gif] }
+  validates :avatar, content_type: { in: %w[image/jpg image/png image/gif] }
 
   # リサイズ済みのプロフィール画像を返す
   def display_avatar

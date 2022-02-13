a07da25b18a618836b557b47e6911a0911c7bcdd
sadanora <sadanora.pi@gmail.com>
2022-02-13 11:47:41 +0900

    remove: updateアクションのカスタマイズ削除
    必要ないことがわかったので削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def update
-    if account_update_params[:avatar].present?
-      resource.avatar.attach(account_update_params[:avatar])
-    end
-  end
 end

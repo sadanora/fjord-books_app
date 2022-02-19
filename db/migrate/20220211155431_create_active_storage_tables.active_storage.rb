69f42b9af49d206f4ba51659a7c7a0a3f6fa429c
sadanora <sadanora.pi@gmail.com>
2022-02-19 22:02:16 +0900

    rubocop
    migrationファイルにtimestampカラム追加

--- db/migrate/20220211155431_create_active_storage_tables.active_storage.rb
+++ db/migrate/20220211155431_create_active_storage_tables.active_storage.rb
@@ -30,6 +30,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
     create_table :active_storage_variant_records do |t|
       t.belongs_to :blob, null: false, index: false
       t.string :variation_digest, null: false
+      t.timestamps
 
       t.index %i[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2022_02_11_155431) do
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.integer "blob_id", null: false
     t.string "variation_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 

47c2aa8ff7b63942d4851253a470065fe72e30a0
sadanora <sadanora.pi@gmail.com>
2022-02-23 21:19:41 +0900

    add: followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "UserFollow",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "UserFollow",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
   has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   def follow(other_user)
     followings << other_user

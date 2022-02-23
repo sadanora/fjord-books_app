71a2abbcc22580b9772791501a724e2fc0e6e91e
sadanora <sadanora.pi@gmail.com>
2022-02-23 20:42:48 +0900

    UserとUserFollowの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_relationships, class_name:  "UserFollow",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
 end
